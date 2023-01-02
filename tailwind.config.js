c3a6ddfece3d16f0f6924b736751e53b66dd87d4
muhammadfuaad <fuaad29@gmail.com>
2023-01-02 20:02:19 +0500

    some colors added in tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'blue': '#00ADE9',
+        'night-blue': '#223879',
+        'navy-blue': '#006BA3',
+        
+      }
+    },
   },
   variants: {
     extend: {},
