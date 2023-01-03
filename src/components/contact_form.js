28a198ea4eb0e326bc38e64de6e0e98005596439
muhammadfuaad <fuaad29@gmail.com>
2023-01-03 22:23:29 +0500

    fixed placeholder position in contact-form

--- src/components/contact_form.js
+++ src/components/contact_form.js
@@ -4,17 +4,33 @@ function Contact_form() {
   return (
   <div className="card contact-form relative">
     <p className="card__heading">Contact Form</p>
-    <input className="input" type="text" placeholder="Name*"></input>
-    <input className="input" type="text" placeholder="Email*"></input>
-    <input className="input" type="text" placeholder="Phone*"></input>
-    <input className="input" type="text" placeholder="Subject*"></input>
-    <input className="input message" type="text" placeholder="Message*"></input>
+    <div className="input-container">
+      <input className="input" type="text" id="name" required></input>
+      <label for="name">Name*</label>
+    </div>
+    <div className="input-container">
+      <input className="input" type="text" id="email" required></input>
+      <label for="email">email*</label>
+    </div>
+    <div className="input-container">
+      <input className="input" type="text" id="phone" required></input>
+      <label for="phone">phone*</label>
+    </div>
+    <div className="input-container">
+      <input className="input" type="text" id="subject" required></input>
+      <label for="subject">subject*</label>
+    </div>
+    <div className="input-container">
+      <input className="input message" type="text" id="message" required></input>
+      <label for="message">message*</label>
+    </div>
+    
     <div className="flex space-x-2">
     <a href=""><img className="w-5 mt-1" src="/Icons/icon_toogle-off.svg" alt="logo_facebook" /></a>
       <p className="disclaimer">I agree to <span className="highlights">Terms & Conditions </span>
       and <span className="highlights">Privacy Policy</span></p>
     </div>
-    <button className="btn btn--primary">Submit</button>
+    <button className="btn btn--primary">Submit</button>   
   </div>
   );
 }
