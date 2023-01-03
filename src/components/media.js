cac7fa1e35ffc94193430491fb63732a6292f89e
muhammadfuaad <fuaad29@gmail.com>
2023-01-03 15:16:57 +0500

    code

---
+++ src/components/media.js
@@ -0,0 +1,30 @@
+function media() {
+
+
+  return (
+    <>
+      <div class="flex media-section justify-between">
+        <div className="media">
+          
+          <div className="media__body">
+            <h3 className="media__title">Phone</h3>
+            <p>
+            03300579957
+            </p>
+          </div>
+        </div>
+        <div className="media">
+          
+          <div className="media__body">
+            <h3 className="media__title">Email</h3>
+            <p>
+            03300579957
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+    
+  );
+}
+export default media;
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import Switch from '../components/switch/switch';
 import Header from '../components/header';
-import My_card from '../components/my_card';
+import Contact_form from '../components/contact_form';
+import Media from '../components/media'
 import Footer from '../components/footer';
 
 
@@ -18,36 +19,23 @@ function Home() {
       <p className="page-heading">Contact</p>
       
     </div>
-    <section className="container flex flex-col space-y-8">
-      <div className="basis-3/5 flex flex-col space-y-8">
-        <p className="navigator">Home / Contact</p>
-        <p className="page__heading">Contact</p>
-        <p className="card__heading">Got a Question?</p>
-        <p className="body__text">Aid Humanity is a thoughtful, caring community of like-minded individuals ready to give back and make a difference. If you’d like to know more about what we do, or have a question about how you can help, all you have to do is ask.</p>
+    <section className="block">
+      <div class="flex">
+        <div class="basis-1/2 flex flex-col space-y-8 page-content">
+          <div className="flex flex-col space-y-8">
+            <p className="navigator">Home / Contact</p>
+            <p className="page__heading">Contact</p>
+            <p className="card__heading">Got a Question?</p>
+            <p className="body__text">Aid Humanity is a thoughtful, caring community of like-minded individuals ready to give back and make a difference. If you’d like to know more about what we do, or have a question about how you can help, all you have to do is ask.</p>
         
-        <div class="media">
-          <div class="media__image">
-            <svg class="icon icon--primary">
-              <img src="" ></img>
-            </svg>
-          </div>
-          <div class="media__body">
-            <h3 class="media__title">Easy Start & Managed Updates</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam quisquam,
-              ex nostrum vero voluptates dicta excepturi vel perspiciatis consequuntur
-              ab.
-            </p>
+            <Media />
+        
+            <p className="card__heading">Company Details</p>
+            <p className="body__text">Aid Humanity, Unit 9, Twelve o’clock Court, Sheffield, S4 7WW</p>
           </div>
         </div>
-
-        
-        <p className="card__heading">Company Details</p>
-        <p className="body__text">Aid Humanity, Unit 9, Twelve o’clock Court, Sheffield, S4 7WW</p>
-
+        <Contact_form />
       </div>
-
-      <My_card />
       
       
       
