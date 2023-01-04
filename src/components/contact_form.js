22be2d4301d081970bc09b3db50f2bdf2e26971e
muhammadfuaad <fuaad29@gmail.com>
2023-01-04 21:24:19 +0500

    modified contact_form

--- src/components/contact_form.js
+++ src/components/contact_form.js
@@ -4,29 +4,32 @@ function Contact_form() {
   return (
   <div className="card contact-form relative">
     <p className="card__heading">Contact Form</p>
-    <div className="input-container">
-      <input className="input" type="text" id="name" required></input>
-      <label for="name">Name*</label>
+    <div class="form__inputs">
+      <div className="input-container">
+        <input className="input" type="text" id="name" required></input>
+        <label for="name">Name*</label>
+      </div>
+      <div className="input-container">
+        <input className="input" type="text" id="email" required></input>
+        <label for="email">Email*</label>
+      </div>
+      <div className="input-container">
+        <input className="input" type="text" id="phone" required></input>
+        <label for="phone">Phone*</label>
+      </div>
+      <div className="input-container">
+        <input className="input" type="text" id="subject" required></input>
+        <label for="subject">Subject*</label>
+      </div>
+      <div className="input-container">
+        <input className="input message" type="text" id="message" required></input>
+        <label for="message">Message*</label>
+      </div>
     </div>
-    <div className="input-container">
-      <input className="input" type="text" id="email" required></input>
-      <label for="email">Email*</label>
-    </div>
-    <div className="input-container">
-      <input className="input" type="text" id="phone" required></input>
-      <label for="phone">Phone*</label>
-    </div>
-    <div className="input-container">
-      <input className="input" type="text" id="subject" required></input>
-      <label for="subject">Subject*</label>
-    </div>
-    <div className="input-container">
-      <input className="input message" type="text" id="message" required></input>
-      <label for="message">Message*</label>
-    </div>
+    <p className="character-limit">9/999</p>
     
-    <div className="flex space-x-2">
-    <a href=""><img className="w-5 mt-1" src="/Icons/icon_toogle-off.svg" alt="logo_facebook" /></a>
+    <div className="flex space-x-2 my-2">
+      <a href=""><img className="w-5 mt-1" src="/Icons/icon_toogle-off.svg" alt="logo_facebook" /></a>
       <p className="disclaimer">I agree to <span className="highlights">Terms & Conditions </span>
       and <span className="highlights">Privacy Policy</span></p>
     </div>
