c5574a67c09221b2cc269abf648d471efe70dc44
muhammadfuaad <fuaad29@gmail.com>
2023-01-04 21:25:36 +0500

    corrected font-size of email id

--- src/components/media.js
+++ src/components/media.js
@@ -6,28 +6,20 @@ function media() {
       <div class="flex media-section justify-between">
         <div className="media">
           <div className="media__image">
-            
-              <img className="icon" src="/Icons/icon_phone-volume.svg"></img>
-            
+            <img className="icon" src="/Icons/icon_phone-volume.svg"></img>
           </div>
           <div className="media__body">
             <h3 className="media__title">Phone</h3>
-            <p>
-            03300579957
-            </p>
+            <p className="media__content email">03300579957</p>
           </div>
         </div>
         <div className="media">
           <div className="media__image">
-            
-              <img className="icon" src="/Icons/icon_envelope-open.svg"></img>
-            
+            <img className="icon" src="/Icons/icon_envelope-open.svg"></img>
           </div>
           <div className="media__body">
             <h3 className="media__title">Email</h3>
-            <p>
-            03300579957
-            </p>
+            <p className="media__content email">info@aidhumanity.co.uk</p>
           </div>
         </div>
       </div>
